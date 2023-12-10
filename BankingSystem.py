5ee01776a03ccede7c936a8fe01ee2abe64bad72
Maliha Nawar <87635996+Maliha-Nawar@users.noreply.github.com>
2023-12-11 01:44:56 +0600

    Update BankingSystem.py

--- BankingSystem.py
+++ BankingSystem.py
@@ -1,31 +1,49 @@
-bank={"Shomita":34000,"Kaiser":90000,"Parul":12000,"Kakoli":78000}
-print("Welcome to PLC Bank")
-print("Press->")
-print("1) to view balance")
-print("2) to view Bank's full information")
-print("3) to edit balance")
-print("4) to exit")
-ans=int(input("Your ans: "))
-if ans==1:
-    name=input("Please insert user's name here : ")
-    if name in bank.keys():
-        print(name+"'s bank balance is "+str(bank[name]))
-    else:
-        yn=input("We don't have any account by this name. Would you like to create an account by "+name+"?")
-        if yn=="yes":
-            balance=input("Please, insert the balance here: ")
-            balance=bank[name]
-            print("New data has been successfully inserted!")
+bank={"Shomita":34000,
+      "Kaiser":90000,
+      "Parul":12000,
+      "Ragib":987870,
+      "Jinad":900000,
+      "Kakoli":78000}
+
+while True:
+    print("Welcome to PLC Bank")
+    print("Press->")
+    print("1) to view balance")
+    print("2) to view Bank's full information")
+    print("3) to edit balance")
+    print("4) to exit")
+    ans = (input("Your ans: "))
+    if ans=="1":
+        name=input("Please insert user's name here : ")
+        if name in bank.keys():
+            print(name+"'s bank balance is "+str(bank[name]))
         else:
-            print("ok. Thank you for choosing PLC Bank")
-elif ans==2:
-    print(bank.items())
-elif ans==3:
-    name=input("Please, enter the user name here: ")
-    newbalance = input("New balance?-> ")
-    newbalance = bank[name]
-    print("Data has been successfully edited")
-elif ans==4:
-    print("Thank you for choosing PLC1 Bank!")
-else:
-    print("Please give appropriate indication")
+            yn=input("We don't have any account by this name. Would you like to create an account by "+name+"?")
+            if yn=="yes":
+                balance=input("Please, insert the balance here: ")
+                bank[name]=balance
+                print("New data has been successfully inserted!")
+            else:
+                print("ok. Thank you for choosing PLC Bank")
+    elif ans=="2":
+        print(bank.items())
+    elif ans=="3":
+        name=input("Please, enter the user name here: ")
+        if name in bank.keys():
+            newbalance = input("New balance?-> ")
+            newbalance = bank[name]
+            print("Data has been successfully edited")
+        else:
+            yn=input("We don't have any account by this name. Would you like to create an account by "+name+"? ")
+            if yn=="yes":
+              balance=input("Please, insert the balance here: ")
+              bank[name]=balance
+              print("New data has been successfully inserted!")
+            else:
+              print("ok. Thank you for choosing PLC Bank")
+
+    elif ans=="4":
+        print("Thank you for choosing PLC Bank!")
+        break
+    else:
+        print("Please give appropriate indication")
